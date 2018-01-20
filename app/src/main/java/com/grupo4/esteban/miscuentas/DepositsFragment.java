613525ce912794e6d59dfb476a720475958d479d
Esteban <estebanizca93@gmail.com>
2018-01-20 13:16:04 +0100

    VERSION FINAL PARA ENTREGAR. - Control de fallos y formateo de las cantidades (dos decimales). - TODO COMENTADO - TODO FUNCIONANDO. - Widget GastosTotales implementado. - Widget medianamente implemetando, comprendele bien y acabar la funcionalidad que muestre gastos totales. - Simbolo de € o $ según el idioma implementado - NavBar funcinal y totalemnte implementada. - Cambiados los RelativeLayouts por ConstrainLayouts - Añadido ViewBinder para cambiar colores según si es tipo gasto o ingreso en la lista de mis cuentas. - Mejorados los mensajes de (Añadidos DialogsBox), cuando se elimina base de datos pide confirmación, cuando se sobrepasa el límite y cuando se piden gastos totales. - Añadido el SharedPrefers para establecer un límite de gastos (por defecto 10.000) y que comprueba si se sobrepasa licho límite al hacer un nuevo ingreso - Completada toda la internacionalización con los idiomas por defecto el Inglés y añadido el Español. - Comentado todo el código.

--- app/src/main/java/com/grupo4/esteban/miscuentas/DepositsFragment.java
+++ app/src/main/java/com/grupo4/esteban/miscuentas/DepositsFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 /**
  * Created by Esteban on 30/12/2017.
@@ -39,18 +43,34 @@ public class DepositsFragment extends Fragment implements View.OnClickListener{
                 //Se recogen en variables los datos concepto y valor introducidos en la vista
                 String concept = txtConcept.getText().toString();
                 String value = numValue.getText().toString();
-                double numValueDouble = Double.parseDouble(value);
+                //Sentencia IF que comprueba que se rellenan todos los campos
+                if (TextUtils.isEmpty(concept) || TextUtils.isEmpty(value))
+                    Snackbar.make(DepositsFragment.this.getView(), getResources().getString(R.string.fieldsEmpty), Snackbar.LENGTH_LONG).show();
+                else {
+                    //Sentencia IF que comprueba que la cantidad está en formato correcto
+                    if (!Character.isDigit(value.charAt(0)))
+                        Snackbar.make(DepositsFragment.this.getView(), getResources().getString(R.string.valueError), Snackbar.LENGTH_LONG).show();
+                    //Si todas las comprobaciones son correctas, se ejecutan las siguientes funciones
+                    else {
+                        //Funciones y variables para formatear el valor a dos decimales.
+                        double numValueDouble = Double.parseDouble(value);
+                        DecimalFormatSymbols separadoresPersonalizados = new DecimalFormatSymbols();
+                        separadoresPersonalizados.setDecimalSeparator('.');
+                        DecimalFormat dosdecimales = new DecimalFormat("#.##", separadoresPersonalizados);
+                        numValueDouble = Double.parseDouble(dosdecimales.format(numValueDouble));
 
-                //Se llaman a las funciones Set del Activity para que después sean insertados los datos en la BBDD
-                ((DepositsActivity)getActivity()).setTxtConcept(concept);
-                ((DepositsActivity)getActivity()).setNumValue(numValueDouble);
-                //Una vez "Seteados los datos" se llama a la función insertRegister del activity que insertará de forma correcta los datos en la BBDD.
-                ((DepositsActivity)getActivity()).insertRegister();
-                //Se muestra un mensaje de éxito si el registro se ha llevado a cabo correctamente
-                Toast.makeText(DepositsFragment.this.getActivity(), getResources().getString(R.string.succesDeposit), Toast.LENGTH_LONG).show();
-                //Se lanza automáticamente de nuevo el activity al menú principal de la aplicación, que en este caso es MainActivity.
-                Intent MainActivity = new Intent(v.getContext(), MainActivity.class);
-                startActivityForResult(MainActivity, 0);
+                        //Se llaman a las funciones Set del Activity para que después sean insertados los datos en la BBDD
+                        ((DepositsActivity) getActivity()).setTxtConcept(concept);
+                        ((DepositsActivity) getActivity()).setNumValue(numValueDouble);
+                        //Una vez "Seteados los datos" se llama a la función insertRegister del activity que insertará de forma correcta los datos en la BBDD.
+                        ((DepositsActivity) getActivity()).insertRegister();
+                        //Se muestra un mensaje de éxito si el registro se ha llevado a cabo correctamente
+                        Toast.makeText(DepositsFragment.this.getActivity(), getResources().getString(R.string.succesDeposit), Toast.LENGTH_LONG).show();
+                        //Se lanza automáticamente de nuevo el activity al menú principal de la aplicación, que en este caso es MainActivity.
+                        Intent MainActivity = new Intent(v.getContext(), MainActivity.class);
+                        startActivityForResult(MainActivity, 0);
+                    }
+                }
             }
         });
 
--- app/src/main/java/com/grupo4/esteban/miscuentas/ExpensesFragment.java
+++ app/src/main/java/com/grupo4/esteban/miscuentas/ExpensesFragment.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -46,50 +49,65 @@ public class ExpensesFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onClick(final View v) { //Acciones que se llevan a cabo al pulsar el botón.
                 //Se recogen en variables los datos concepto y valor introducidos en la vista
+                Snackbar snackBar;
                 String concept = txtConcept.getText().toString();
                 String value = numValue.getText().toString();
-                double numValueDouble = Double.parseDouble(value);
-                String spendLimitS = prefs.getString("expensesLimit", "10000");
-                double spendLimit = Double.parseDouble(spendLimitS);
-                Log.d(TAG, "LIMITE:" + spendLimitS.toString());
-                //float spendLimit = prefs.getFloat("expensesLimit", 10000);
 
-                //Funciones
-
-                //Se llaman a las funciones Set del Activity para que después sean insertados los datos en la BBDD
-                ((ExpensesActivity) getActivity()).setTxtConcept(concept);
-                ((ExpensesActivity) getActivity()).setNumValue(numValueDouble);
-                //Una vez "Seteados los datos" se llama a la función insertRegister del activity que insertará de forma correcta los datos en la BBDD.
-                ((ExpensesActivity) getActivity()).insertRegister();
-                //Se muestra un mensaje de éxito si el registro se ha llevado a cabo correctamente
-                Toast.makeText(ExpensesFragment.this.getActivity(), getResources().getString(R.string.succesSpend), Toast.LENGTH_LONG).show();
-                //Sentencia IF para mostrar un cuadro de diálogo si se sobrepasado el límite establecido en las preferencias compartidas.
-                if (((ExpensesActivity) getActivity()).getAllExpenses() >= spendLimit) {
-                    AlertDialog.Builder builder;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        builder = new AlertDialog.Builder(getActivity(), android.R.style.Theme_Material_Dialog_Alert);
-                    } else {
-                        builder = new AlertDialog.Builder(getActivity());
-                    }
-                    builder.setTitle(getResources().getString(R.string.titleLimit))
-                            .setMessage(getResources().getString(R.string.limitExceeded))
-                            .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    //Se lanza automáticamente de nuevo el activity al menú principal de la aplicación, que en este caso es MainActivity.
-                                    Intent MainActivity = new Intent(v.getContext(), MainActivity.class);
-                                    startActivityForResult(MainActivity, 0);
-                                }
-                            })
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show();
+                //Sentencia IF que comprueba que se rellenan todos los campos
+                if (TextUtils.isEmpty(concept) || TextUtils.isEmpty(value)) {
+                    Snackbar.make(ExpensesFragment.this.getView(), getResources().getString(R.string.fieldsEmpty), Snackbar.LENGTH_LONG).show();
                 } else {
-                    //Se lanza automáticamente de nuevo el activity al menú principal de la aplicación, que en este caso es MainActivity.
-                    Intent MainActivity = new Intent(v.getContext(), MainActivity.class);
-                    startActivityForResult(MainActivity, 0);
+                    //Sentencia IF que comprueba que la cantidad está en formato correcto
+                    if (!Character.isDigit(value.charAt(0)))
+                        Snackbar.make(ExpensesFragment.this.getView(), getResources().getString(R.string.valueError), Snackbar.LENGTH_LONG).show();
+                    //Si todas las comprobaciones son correctas, se ejecutan las siguientes funciones
+                    else {
+                        //Funciones y variables para formatear el valor a dos decimales.
+                        double numValueDouble = Double.parseDouble(value);
+                        DecimalFormatSymbols separadoresPersonalizados = new DecimalFormatSymbols();
+                        separadoresPersonalizados.setDecimalSeparator('.');
+                        DecimalFormat dosdecimales = new DecimalFormat("#.##", separadoresPersonalizados);
+                        numValueDouble = Double.parseDouble(dosdecimales.format(numValueDouble));
+                        //Variables de preferencias compartidas
+                        String spendLimitS = prefs.getString("expensesLimit", "10000");
+                        double spendLimit = Double.parseDouble(spendLimitS);
+                        //Se llaman a las funciones Set del Activity para que después sean insertados los datos en la BBDD
+                        ((ExpensesActivity) getActivity()).setTxtConcept(concept);
+                        ((ExpensesActivity) getActivity()).setNumValue(numValueDouble);
+                        //Una vez "Seteados los datos" se llama a la función insertRegister del activity que insertará de forma correcta los datos en la BBDD.
+                        ((ExpensesActivity) getActivity()).insertRegister();
+                        //Se muestra un mensaje de éxito si el registro se ha llevado a cabo correctamente
+                        Toast.makeText(ExpensesFragment.this.getActivity(), getResources().getString(R.string.succesSpend), Toast.LENGTH_LONG).show();
+                        //Sentencia IF para mostrar un cuadro de diálogo si se sobrepasado el límite establecido en las preferencias compartidas.
+                        if (((ExpensesActivity) getActivity()).getAllExpenses() >= spendLimit) {
+                            AlertDialog.Builder builder;
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                builder = new AlertDialog.Builder(getActivity(), android.R.style.Theme_Material_Dialog_Alert);
+                            } else {
+                                builder = new AlertDialog.Builder(getActivity());
+                            }
+                            builder.setTitle(getResources().getString(R.string.titleLimit))
+                                    .setMessage(getResources().getString(R.string.limitExceeded))
+                                    .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            //Se lanza automáticamente de nuevo el activity al menú principal de la aplicación, que en este caso es MainActivity.
+                                            Intent MainActivity = new Intent(v.getContext(), MainActivity.class);
+                                            startActivityForResult(MainActivity, 0);
+                                        }
+                                    })
+                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .show();
+                        } else {
+                            //Se lanza automáticamente de nuevo el activity al menú principal de la aplicación, que en este caso es MainActivity.
+                            Intent MainActivity = new Intent(v.getContext(), MainActivity.class);
+                            startActivityForResult(MainActivity, 0);
+                        }
+                    }
                 }
             }
         });
+
 
         return view;
     }
